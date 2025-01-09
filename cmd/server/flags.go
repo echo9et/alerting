a0df7757529a64f9abd64755a05e5dbc00d06b2e
Evgeniy Vasilev <echo9et@yandex.ru>
2025-01-09 13:39:42 +0300

    fix agent

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"flag"
+	"os"
+)
+
+var addrServer *string = flag.String("a", "localhost:8080", "server and port to run server")
+
+func ParseFlags() {
+	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
+		*addrServer = envRunAddr
+	}
+
+	flag.Parse()
+}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-	"flag"
-	"os"
-
 	"github.com/echo9et/alerting/cmd/server/coreserver"
 	"github.com/echo9et/alerting/cmd/server/storage"
 )
-
-var addrServer *string = flag.String("a", "localhost:8080", "server and port to run server")
-
-func ParseFlags() {
-	if envRunAddr := os.Getenv("ADDRESS"); envRunAddr != "" {
-		*addrServer = envRunAddr
-	}
-
-	flag.Parse()
-}
 
 func main() {
 	ParseFlags()
