772bc059c2934597d7e439e750670c2b7c007601
Evgeniy Vasilev <echo9et@yandex.ru>
2025-01-27 00:37:52 +0300

    fix: iter8

--- internal/server/coreserver/core.go
+++ internal/server/coreserver/core.go
@@ -79,6 +79,9 @@ func metricsHandle(w http.ResponseWriter, r *http.Request, s handlers.Storage) {
 	for k, v := range metrics {
 		w.Write([]byte(fmt.Sprintln(k, v)))
 	}
+	if r.Header.Get("Content-Type") == "text/plain" {
+		w.Header().Set("Content-Type", "text/html")
+	}
 	w.WriteHeader(http.StatusOK)
 }
 
