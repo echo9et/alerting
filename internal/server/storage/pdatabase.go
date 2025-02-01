5001b45442635f79d21db9bbab576086cce19e68
Evgeniy Vasilev <echo9et@yandex.ru>
2025-02-01 22:01:51 +0300

    iter 10

--- internal/server/storage/pdatabase.go
+++ internal/server/storage/pdatabase.go
@@ -2,10 +2,10 @@ package storage
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
-	"net"
-	"time"
+	"os"
+
+	"github.com/jackc/pgx"
 )
 
 type Base struct {
@@ -20,27 +20,12 @@ func NewPDatabase(a string) *Base {
 
 func (b *Base) Ping() bool {
 	fmt.Println(b.addr)
-	ip, port, err := net.SplitHostPort(b.addr)
-	if err != nil {
-		fmt.Println("error parse addr")
-		return false
-	}
-	cmd := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		ip, port, `postgres`, `postgres`, `postgres`)
-	db, err := sql.Open("pgx", cmd)
-	if err != nil {
-		fmt.Println("dont open db", cmd)
-		return false
-	}
-	defer db.Close()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-	if err = db.PingContext(ctx); err != nil {
-		fmt.Println("timeout")
+	_, err := pgx.Connect(context.Background(), b.addr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		return false
 	}
-	fmt.Println("database connect")
 
 	return true
 }
